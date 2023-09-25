364f3addcc115f2556f4e58b57179e0a25024a1a
ulmo369 <a01366802@itesm.mx>
2023-09-25 12:32:10 -0600

    Add controll configuration for motors and recollector

---
+++ Robot15/include/robot-config.h
@@ -0,0 +1,27 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern controller Controller1;
+//Derecha
+extern motor RightDriveA;
+extern motor RightDriveB;
+
+//Izquierda
+extern motor LeftDriveA;
+extern motor LeftDriveB;
+
+extern motor_group LeftDriveSmart;
+extern motor_group RightDriveSmart;
+
+//Recolector 15(alitas)
+extern pneumatics Indexer;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
